3328efe4ac68f62ed0c250665ced1427f7178d53
PEDRO FILHO <fischer.pedro.social@gmail.com>
2026-04-06 22:25:45 -0300

    create new class ProductTest to validate basic model entry

--- Models/Product.cs
+++ Models/Product.cs
@@ -19,30 +19,70 @@ namespace Models
             Id = Guid.NewGuid();
             CreatedAt = DateTime.UtcNow;
 
-            this.Name = name;
-            this.Description = description;
-            this.Price = price;
-            this.Category = category;
-            this.Stock = stock;
+            this.SetName(name);
+            this.SetDescription(description);
+            this.SetPrice(price);
+            this.SetCategory(category);
+            this.SetStock(stock);
+            
             this.Tags = tags ?? new List<string>();
         }
         public void SetName(string name)
         {
-            if (name.Length < 3)
+            if (string.IsNullOrEmpty(name)||name.Length < 3)
             {
                 throw new ArgumentException("Invalid name");
             }
             this.Name = name;
         }
+        public void SetDescription(string description)
+        {
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new ArgumentException("Description is required");
+            }
+            this.Description = description;
+        }
 
         public void SetPrice(decimal price)
         {
-            if (price < 0) 
+            if (price <= 0) 
             { 
                 throw new ArgumentException("Price needs great than zero"); 
             } 
             this.Price = price;
         }
+
+
+        public void SetStock(int stock)
+        {
+            if (stock < 0)
+            {
+                throw new ArgumentException("Stock cannot be negative");
+            }
+            this.Stock = stock;
+        }
+
+        public void SetCategory(string category)
+        {
+            if (string.IsNullOrEmpty(category))
+            {
+                throw new ArgumentException("Category is required");
+            }
+            this.Category = category;
+        }
+
+        public void AddTag(string tag)
+        {
+            if (string.IsNullOrWhiteSpace(tag))
+                throw new ArgumentException("Invalid tag");
+
+            if (Tags.Contains(tag))
+                return;
+
+            Tags.Add(tag);
+        }
+
         public void MarkAsUpdated() 
         { 
             this.UpdatedAt = DateTime.UtcNow;
--- Tests/ProductTest.cs
+++ Tests/ProductTest.cs
@@ -13,6 +13,18 @@ namespace Tests
      */
     public class ProductTest
     {
+        private Product CreateNewProductHelper()
+        {
+            return new Product(
+               "Produto",
+               "Descrição válida longa",
+               100,
+               "categoria",
+               10,
+               new List<string>()
+           );
+        }
+
         [Fact]
         public void ShouldCreateNewValidProduct()
         {
@@ -22,10 +34,76 @@ namespace Tests
                4500,
                "smartphone",
                10,
-               []
+               new List<string>()
 
             );
 
+            Assert.Equal("iPhone 13", newProduct.Name);
+            Assert.Equal(4500, newProduct.Price);
+            Assert.Equal(10, newProduct.Stock);
         }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenPriceIsZero()
+        {
+            Assert.Throws<ArgumentException>(() =>
+               new Product(
+                   "Produto",
+                   "Descrição válida longa",
+                   0,
+                   "categoria",
+                   10,
+                   new List<string>()
+               )
+           );
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenNameIsTooShort()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new Product(
+                    "ab",
+                    "Descrição válida longa",
+                    100,
+                    "categoria",
+                    10,
+                    new List<string>()
+                )
+            );
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenStockIsNegative()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new Product(
+                    "ab",
+                    "Descrição válida longa",
+                    100,
+                    "categoria",
+                    -1,
+                    new List<string>()
+                )
+            );
+        }
+
+
+        [Fact]
+        public void ShouldThrowExceptionWhenCategoryIsNull()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new Product(
+                    "ab",
+                    "Descrição válida longa",
+                    100,
+                    "",
+                    10,
+                    new List<string>()
+                )
+            );
+        }
+
+
     }
 }
